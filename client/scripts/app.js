74d1db21191ee255efc6a29ded4239b264be5d3f
dorktobes <t.hoshman@gmail.com>
2017-10-20 15:49:58 -0700

    implemented send()

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,20 +1,39 @@
 // YOUR CODE HERE:
 // debugger;
+var app = {
+  init: function() {},
+  
+  send: function (message) {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+      type: 'POST',
+      data: message,
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent by Seanzy');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  }, 
+  fetch: function () {
+    $.get('http://parse.sfm8.hackreactor.com/chatterbox/classes/messages', console.dir);
+  },
+  
+};
 
 
-// $.ajax({
-//   // This is the url you should use to communicate with the parse API server.
-//   url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
-//   type: 'POST',
-//   data: JSON.stringify(message),
-//   contentType: 'application/json',
-//   success: function (data) {
-//     console.log('chatterbox: Message sent by Seanzy');
-//   },
-//   error: function (data) {
-//     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-//     console.error('chatterbox: Failed to send message', data);
-//   };
+
+//$(window).onready(function () {});
+
+  
+  
+  
+  
+  
   
   /*
   A common practice (which we adopt here) is to prevent that by storing our API keys in a special file config.js (referenced in client/index.html) that we add to our .gitignore so that it's never committed to our repo. This means that after you clone down the repo, before running the app, you must re-create that special file and add your API keys to it or the app won't run.
