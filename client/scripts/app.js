2d23db99c8743d794ff029a475d178493b90e8a5
dorktobes <t.hoshman@gmail.com>
2017-10-20 11:47:26 -0700

    bower installed

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,21 @@
 // YOUR CODE HERE:
+// debugger;
+
+
+// $.ajax({
+//   // This is the url you should use to communicate with the parse API server.
+//   url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
+//   type: 'POST',
+//   data: JSON.stringify(message),
+//   contentType: 'application/json',
+//   success: function (data) {
+//     console.log('chatterbox: Message sent by Seanzy');
+//   },
+//   error: function (data) {
+//     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+//     console.error('chatterbox: Failed to send message', data);
+//   };
+  
+  /*
+  A common practice (which we adopt here) is to prevent that by storing our API keys in a special file config.js (referenced in client/index.html) that we add to our .gitignore so that it's never committed to our repo. This means that after you clone down the repo, before running the app, you must re-create that special file and add your API keys to it or the app won't run.
+  */
