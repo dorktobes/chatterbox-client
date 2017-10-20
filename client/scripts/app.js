65fb9515deee5ff55d565d399e7e8c0b5a9b152a
dorktobes <t.hoshman@gmail.com>
2017-10-20 13:20:44 -0700

    added given API keys to comments

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,7 +4,7 @@
 
 // $.ajax({
 //   // This is the url you should use to communicate with the parse API server.
-//   url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
+//   url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
 //   type: 'POST',
 //   data: JSON.stringify(message),
 //   contentType: 'application/json',
